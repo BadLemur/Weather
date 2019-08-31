a65970d7d516550f3f2288b31e909872d375da25
badlemur <godfire@list.ru>
2019-08-31 16:43:12 +0400

    правка работы toolbar

--- app/src/main/java/com/example/weather/app/activities/main/MainActivity.java
+++ app/src/main/java/com/example/weather/app/activities/main/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.weather.app.activities.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.example.weather.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends MvpAppCompatActivity implements View_MainActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.main_activity);
+        this.setContentView(R.layout.main_activity2);
+        ButterKnife.bind(this);
+        setActionBar(toolbar);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        return true;
     }
 }
