6e86c208bcba81ebd1718535721c60f5c8113a9b
badlemur <godfire@list.ru>
2019-09-20 02:35:14 +0400

    удалил 3 модуля в mainActivity, упростил код

--- app/src/main/java/com/example/weather/app/activities/main/model/onFirstStart/doCreateDB/iDoCreateDB.java
+++
@@ -1,9 +0,0 @@
-package com.example.weather.app.activities.main.model.onFirstStart.doCreateDB;
-
-import io.reactivex.disposables.Disposable;
-
-public interface iDoCreateDB {
-
-    Disposable createDB();
-
-}
--- app/src/main/java/com/example/weather/data/dagger/component/MainAppComponent.java
+++ app/src/main/java/com/example/weather/data/dagger/component/MainAppComponent.java
@@ -5,8 +5,6 @@ import com.example.weather.app.activities.findCity.presenter.PresenterFindCity;
 import com.example.weather.app.activities.main.model.addOldChoiceCity.AddOldChoiceCity;
 import com.example.weather.app.activities.main.model.doViewModel.DoViewModel;
 import com.example.weather.app.activities.main.model.onFirstStart.OnFirstStart;
-import com.example.weather.app.activities.main.model.onFirstStart.doCreateDB.DoCreateDB;
-import com.example.weather.app.activities.main.model.onFirstStart.doCreateDB.parserFile.NewParserJson;
 import com.example.weather.app.fragments.weatherCity.model.ModelWeatherCity;
 import com.example.weather.app.fragments.weatherCity.model.ReturnTypeWeather;
 import com.example.weather.data.DB.parser.ParserJson;
@@ -33,10 +31,6 @@ public interface MainAppComponent {
     void inject(ModelWeatherCity app);
 
     void inject(ReturnTypeWeather app);
-
-    void inject(NewParserJson app);
-
-    void inject(DoCreateDB app);
 
     void inject(OnFirstStart app);
 }
